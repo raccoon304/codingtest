329c7474bc717b632fe2f56e1a4f541eb4d75e38
raccoon304 <99147371+raccoon304@users.noreply.github.com>
2023-09-08 13:12:32 +0900

    [Silver IV] Title: 덱, Time: 76 ms, Memory: 34308 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,42 @@
+import sys 
+from collections import deque
+input = sys.stdin.readline 
+
+n = int(input())
+li = deque()
+
+for i in range(n):
+    command = list(map(str, input().rstrip().split()))
+    ct = command[0]
+    if ct == 'push_front':
+        li.appendleft(command[1])
+    elif ct == 'push_back':
+        li.append(command[1])
+    elif ct == 'pop_front':
+        if len(li) >= 1:
+            print(li.popleft())
+        else:
+            print(-1)
+    elif ct == 'pop_back':
+        if len(li) >= 1:
+            print(li.pop())
+        else:
+            print(-1)
+    elif ct == 'size':
+        print(len(li))
+    elif ct == 'empty':
+        if len(li) == 0 :
+            print(1)
+        else:
+            print(0)
+    elif ct == 'front':
+        if len(li) >= 1:
+            print(li[0])
+        else:
+            print(-1)
+    elif ct == 'back':
+        if len(li) >= 1:
+            print(li[-1])
+        else:
+            print(-1)
+    
