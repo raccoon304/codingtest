ea4f508df6902f42be946e5f804827eea8cbe409
raccoon304 <99147371+raccoon304@users.noreply.github.com>
2023-12-04 17:32:11 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,17 @@
+import sys 
+input = sys.stdin.readline 
+
+L = int(input())
+li = str(input().lstrip().rstrip('\n'))
+li = list(li)
+
+ans = 0 
+
+for i in range(L):
+    num = (ord(li[i])-96) * (31**i)
+    ans += num 
+
+print(ans)
+
+    
+
